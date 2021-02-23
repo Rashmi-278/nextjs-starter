2a6cc1e75e62f295e67f220f1045bbb801c8d421
Rashmi V A <rashmi.va@renderpub.com>
2021-02-23 12:23:33 +0530

    Reinitialized correct repo

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -10,7 +10,7 @@ const faunaClient = new faunadb.Client({
   secret: process.env.FAUNADB_SECRET ?? 'secret',
   scheme: isProduction ? 'https' : 'http',
   domain: isProduction ? 'db.fauna.com' : 'localhost',
-  ...(isProduction ? {} : { port: 8443 }),
+  ...(isProduction ? {} : { port: 3000 }),
 })
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options)
@@ -26,21 +26,21 @@ type User = Partial<{
 
 const options: InitOptions = {
   providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-      scope: 'user:email',
-      // @ts-ignore
-      profile: (profileData) => {
-        return {
-          id: profileData.id,
-          name: profileData.name || profileData.login,
-          email: profileData.email,
-          image: profileData.avatar_url,
-          username: profileData.login,
-        }
-      },
-    }),
+    // Providers.GitHub({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    //   scope: 'user:email',
+    //   // @ts-ignore
+    //   profile: (profileData) => {
+    //     return {
+    //       id: profileData.id,
+    //       name: profileData.name || profileData.login,
+    //       email: profileData.email,
+    //       image: profileData.avatar_url,
+    //       username: profileData.login,
+    //     }
+    //   },
+    // }),
     Providers.Email({
       server: {
         host: process.env.SMTP_HOST,
@@ -52,37 +52,37 @@ const options: InitOptions = {
       },
       from: process.env.SMTP_FROM,
     }),
-    Providers.LinkedIn({
-      clientId: process.env.LINKEDIN_ID,
-      clientSecret: process.env.LINKEDIN_SECRET,
-      scope: 'r_liteprofile',
-      // @ts-ignore
-      profileUrl:
-        'https://api.linkedin.com/v2/me?projection=(id,localizedFirstName,localizedLastName,profilePicture(displayImage~digitalmediaAsset:playableStreams))',
-      // @ts-ignore
-      profile: (profileData) => {
-        const profileImage =
-          profileData?.profilePicture?.['displayImage~']?.elements?.[3]
-            ?.identifiers?.[0]?.identifier ??
-          profileData?.profilePicture?.['displayImage~']?.elements?.[2]
-            ?.identifiers?.[0]?.identifier ??
-          profileData?.profilePicture?.['displayImage~']?.elements?.[1]
-            ?.identifiers?.[0]?.identifier ??
-          profileData?.profilePicture?.['displayImage~']?.elements?.[0]
-            ?.identifiers?.[0]?.identifier ??
-          ''
-        const name =
-          profileData.localizedFirstName + ' ' + profileData.localizedLastName
-        const username = slugify(name + ' ' + profileData.id, { lower: true })
-        return {
-          id: profileData.id,
-          name,
-          email: null,
-          image: profileImage,
-          username,
-        }
-      },
-    }),
+    // Providers.LinkedIn({
+    //   clientId: process.env.LINKEDIN_ID,
+    //   clientSecret: process.env.LINKEDIN_SECRET,
+    //   scope: 'r_liteprofile',
+    //   // @ts-ignore
+    //   profileUrl:
+    //     'https://api.linkedin.com/v2/me?projection=(id,localizedFirstName,localizedLastName,profilePicture(displayImage~digitalmediaAsset:playableStreams))',
+    //   // @ts-ignore
+    //   profile: (profileData) => {
+    //     const profileImage =
+    //       profileData?.profilePicture?.['displayImage~']?.elements?.[3]
+    //         ?.identifiers?.[0]?.identifier ??
+    //       profileData?.profilePicture?.['displayImage~']?.elements?.[2]
+    //         ?.identifiers?.[0]?.identifier ??
+    //       profileData?.profilePicture?.['displayImage~']?.elements?.[1]
+    //         ?.identifiers?.[0]?.identifier ??
+    //       profileData?.profilePicture?.['displayImage~']?.elements?.[0]
+    //         ?.identifiers?.[0]?.identifier ??
+    //       ''
+    //     const name =
+    //       profileData.localizedFirstName + ' ' + profileData.localizedLastName
+    //     const username = slugify(name + ' ' + profileData.id, { lower: true })
+    //     return {
+    //       id: profileData.id,
+    //       name,
+    //       email: null,
+    //       image: profileImage,
+    //       username,
+    //     }
+    //   },
+    // }),
   ],
   adapter: Fauna.Adapter({ faunaClient }),
 
